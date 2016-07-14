e0458356f7b910cd892d7680df44503084b9d413
Macbook <Gotlib@macbook-pro.local>
2016-07-14 11:00:51 +0300

    Initial Commit

---
+++ University/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  University
+//
+//  Created by Gotlib on 14.07.16.
+//  Copyright © 2016 Yog.group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ University/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  University
+//
+//  Created by Gotlib on 14.07.16.
+//  Copyright © 2016 Yog.group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
